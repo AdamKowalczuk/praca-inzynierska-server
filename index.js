6ee8db4153eafc395195e8017cfeb09f3f73c49f
unknown <adam.kowalczuk@poczta.fm>
2022-01-03 15:09:05 +0100

    delete empty spaces

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import userRoutes from "./routes/user.js";
 import dotenv from "dotenv";
-
 const app = express();
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
-
 app.use("/users", userRoutes);
-
 dotenv.config();
 const CONNECTION_URL = process.env.ATLAS_URI;
 const PORT = process.env.PORT || 5000;
-
 app.use(express.static("public"));
 
 mongoose
